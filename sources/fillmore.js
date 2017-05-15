80e602c07b0cc425b56ad1c39255dc18b93ddd92
Owen Lamb <olamb@rzmac6.local>
2017-05-14 21:49:11 -0600

    refresh

--- sources/fillmore.js
+++ sources/fillmore.js
@@ -15,7 +15,8 @@ module.exports = function(done) {
       var $ = cheerio.load(body)
       $('.eventBox').each(function(){
 		var day = $(this).find('.eventInfo .eventDay').text();
-		if(day.length === 1) day = '0'+day
+		if(day.length === 1) day = '0'+day;
+		console.log(day);
 		var month = $(this).find('.eventInfo .eventMonth').text();
 		var monthNum = { 'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6,'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12
 		}
