a26b0787d7c45c96ccbeccb6371fe222e74b74d1
Owen Lamb <olamb@rzmac6.local>
2017-05-21 11:56:19 -0600

    refresh

---
+++ sources/firstbank.js
@@ -0,0 +1,37 @@
+var request = require('request')
+var moment = require('moment')
+var cheerio = require('cheerio')
+
+var url = 'http://www.1stbankcenter.com/events'
+var shows = []
+
+module.exports = function(done) {
+	request({
+      url: url,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:41.0) Gecko/20100101 Firefox/41.0'
+      }
+    }, function(err, response, body) {
+      var $ = cheerio.load(body)
+      $('.entry').each(function(){
+		var date = $(this).find('.date').text().split(',')[1].split(' ');
+		var day = date[2];
+		var month = date[1];
+		var monthNum = { 'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06','Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12' }
+		var year = (new Date()).getFullYear()
+		var time = $(this).find('.time').text();
+		if(day.length === 1) day = '0'+day;
+        var show = {
+          venue: 'Fiddlers Green Ampitheatre',
+          venueURL: 'http://www.1stbankcenter.com',
+          date: year + "-" + monthNum[month] + "-" + day,
+          time: time,
+          url: $(this).find('.title h3 a').attr('href')
+        }
+        show.title = $(this).find('.title h3').text();
+        shows.push(show)
+      })
+
+      done(null, shows)
+    })
+}
