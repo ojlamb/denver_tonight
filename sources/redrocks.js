b99d08bdb514f0ac0762dbc436a633f8db664b21
Owen Lamb <olamb@rzmac6.local>
2017-05-22 22:12:58 -0600

    refresh

--- sources/redrocks.js
+++ sources/redrocks.js
@@ -14,9 +14,8 @@ module.exports = function(done) {
     }, function(err, response, body) {
       var $ = cheerio.load(body)
       $('.redrocks').each(function(){
-
-		  console.log( $(this).find('.post_content_wrapper .post_title').text());
-		// var date = $(this).find('.date').text().split(',')[1].split(' ');
+		  console.log($(this).find('.post_content_wrapper .post_title').text());
+		// var date = $(this).find('.post_content_wrapper .post_title').text();
 		// var day = date[2];
 		// var month = date[1];
 		// var monthNum = { 'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06','Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12' }
@@ -25,12 +24,12 @@ module.exports = function(done) {
 		// if(day.length === 1) day = '0'+day;
         // var show = {
         //   venue: 'The Bluebird Theater',
-        //   venueURL: 'http://www.303tickets.com/RedRocks',
+        //   venueURL: 'http://redrocksonline.com',
         //   date: year + "-" + monthNum[month] + "-" + day,
         //   time: time,
-        //   url: $(this).find('.title h3 a').attr('href')
+        //   url: $(this).find('.post_image a').attr('href');
         // }
-        // show.title = $(this).find('.title h3').text();
+        // show.title = $(this).find('.post_content_wrapper .post_title').text();
         // shows.push(show)
       })
 
