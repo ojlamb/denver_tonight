9ed72495f64e81af927118438120e9bf6e7ade64
Owen Lamb <olamb@rzmac6.local>
2017-06-04 21:03:03 -0600

    refresh

--- sources/citypark.js
+++ sources/citypark.js
@@ -16,6 +16,7 @@ module.exports = function(done) {
       $('.eventlist-event eventlist-event--upcoming').each(function(){
 		var date = $(this).find('.event-date').text();
 		console.log(date);
+		console.log(data);
 		var day = date[2];
 		var month = date[1];
 		var monthNum = { 'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06','Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12' }
