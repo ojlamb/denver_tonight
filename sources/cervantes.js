738f5193b045f5112ddcfa050ead19f6244e854c
Owen Lamb <olamb@rzmac6.local>
2017-06-17 18:25:54 -0600

    refresh

--- sources/cervantes.js
+++ sources/cervantes.js
@@ -20,14 +20,14 @@ module.exports = function(done) {
 		  console.log(date)
 		  var day = date[2];
 		  var month = date[1];
-		  var monthNum = { 'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06','Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12' }
+		  //var monthNum = { 'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06','Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12' }
 		  var year = (new Date()).getFullYear()
 		  if(day.length === 1) day = '0'+day;
           var show = {
 	          venue: 'Cervantes',
 	          venueURL: 'http://www.cervantesmasterpiece.com/',
-	          date: year + "-" + monthNum[month] + "-" + day,
-	          time: 'see website',
+	          date: year + "-" + month + "-" + day,
+	          time: $(this).find('h3 .start-time').text(),
 	          url: 'http://www.cervantesmasterpiece.com' + $(this).find('div a').attr('href')
 	        }
         show.title = $(this).find('div a').text();
