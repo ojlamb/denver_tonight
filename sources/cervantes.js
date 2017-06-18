c7d2de6b3d1ec0555dc0c0a7233f2c5c342b9828
Owen Lamb <olamb@rzmac6.local>
2017-06-17 18:36:20 -0600

    refresh

--- sources/cervantes.js
+++ sources/cervantes.js
@@ -17,23 +17,21 @@ module.exports = function(done) {
 		  if ($(this).find('.date .value-title').attr('title') == undefined) { return; }
 		  var dateRaw = $(this).find('.date .value-title').attr('title').split('T');
 		  var date = dateRaw[0].split('-')
-		  console.log(date)
 		  var day = date[2];
 		  var month = date[1];
-		  //var monthNum = { 'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06','Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12' }
 		  var year = (new Date()).getFullYear()
 		  if(day.length === 1) day = '0'+day;
+		  console.log
           var show = {
 	          venue: 'Cervantes',
 	          venueURL: 'http://www.cervantesmasterpiece.com/',
 	          date: year + "-" + month + "-" + day,
-	          time: $(this).find('.start-time').text(),
+	          time: $(this).find('.start-time').first().text(),
 	          url: 'http://www.cervantesmasterpiece.com' + $(this).find('div a').attr('href')
 	        }
         show.title = $(this).find('div a').text();
 		if(!show.title){ return; }
         shows.push(show)
-		console.log(show);
       })
 
       done(null, shows)
