6762aae5cf8c7e0142071415d092a2c03b82ea2e
Owen Lamb <olamb@rzmac6.local>
2017-06-25 20:52:00 -0600

    refresh

--- index.js
+++ index.js
@@ -58,20 +58,19 @@ fs.readdir(__dirname + '/sources', function(err, dirs) {
             if (show.date === year + '-' + month + '-' + day) {
                 venueHash[show.venue].tonight.push(show)
                 venueHash[show.venue].all.push(show)
-            } else if (show.date == moment(year + '-' + month + '-' + day,'YYYY-MM-DD').add(1, 'days').format('YYYY-MM-DD')) {
-				console.log(true)
+            } else if (show.date == now.add(1, 'days').format('YYYY-MM-DD')) {
 				venueHash[show.venue].one.push(show)
-			} else if (show.date == moment(year + '-' + month + '-' + day,'YYYY-MM-DD').add(2, 'days').format('YYYY-MM-DD')) {
+			} else if (show.date == now.add(2, 'days').format('YYYY-MM-DD')) {
 				venueHash[show.venue].two.push(show)
-			} else if (show.date == moment(year + '-' + month + '-' + day,'YYYY-MM-DD').add(3, 'days').format('YYYY-MM-DD')) {
+			} else if (show.date == now.add(3, 'days').format('YYYY-MM-DD')) {
 				venueHash[show.venue].three.push(show)
-			} else if (show.date == moment(year + '-' + month + '-' + day,'YYYY-MM-DD').add(4, 'days').format('YYYY-MM-DD')) {
+			} else if (show.date == now.add(4, 'days').format('YYYY-MM-DD')) {
 				venueHash[show.venue].four.push(show)
-			} else if (show.date == moment(year + '-' + month + '-' + day,'YYYY-MM-DD').add(5, 'days').format('YYYY-MM-DD')) {
+			} else if (show.date == now.add(5, 'days').format('YYYY-MM-DD')) {
 				venueHash[show.venue].five.push(show)
-			} else if (show.date == moment(year + '-' + month + '-' + day,'YYYY-MM-DD').add(6, 'days').format('YYYY-MM-DD')) {
+			} else if (show.date == now.add(6, 'days').format('YYYY-MM-DD')) {
 				venueHash[show.venue].six.push(show)
-			} else if (show.date == moment(year + '-' + month + '-' + day,'YYYY-MM-DD').add(7, 'days').format('YYYY-MM-DD')) {
+			} else if (show.date == now.add(7, 'days').format('YYYY-MM-DD')) {
 				venueHash[show.venue].seven.push(show)
 			}
         })
